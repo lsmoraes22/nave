5d26dc657e8de20f65b23d97bf99be5a02a3187e
Lucas Sangiorato <lucassangiorato@gmail.com>
2022-11-08 17:11:13 -0300

    boss3

--- class.js
+++ class.js
@@ -3499,6 +3499,15 @@ class boss3 extends enemy {
         this.launchPosAdjust.up.x = 175
         this.launchPosAdjust.up.y = 12
 
+        this.sprites.explosion.sprite.img        = 'boss3'
+        this.sprites.explosion.sprite.cropWidth  = gridSize*9
+        this.sprites.explosion.sprite.cropHeight = gridSize*5
+        this.sprites.explosion.sprite.width      = gridSize*9
+        this.sprites.explosion.sprite.height     = gridSize*5
+        this.sprites.explosion.sprite.assetPos.x = 4
+        this.sprites.explosion.speedAnimation    = 1
+        this.sprites.explosion.sprite.imgFrm     = 45
+
         this.sprites.stopedLeft.sprite.cropWidth = gridSize*9
         this.sprites.stopedLeft.sprite.cropHeight = gridSize*5
         this.sprites.stopedLeft.sprite.width = gridSize*9
